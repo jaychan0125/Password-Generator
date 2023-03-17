e886acf879f32e39fa5b6dc65cba750b72b8d9c1
jaychan0125 <joycechan0125@gmail.com>
2023-03-17 01:07:47 -0400

    added prompts to select characters wanted in password, created variables of strings of the characters wanted because strings can still be acccessed with indexes, and made an empty string to put selected characters in, and made a randomizer

--- script.js
+++ script.js
@@ -13,3 +13,73 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
+
+
+
+
+// var upperCASE = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var upperCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+console.log(upperCASE[5]);
+// var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+//var numbers = '[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]'; 
+var numbers = '0123456789';
+//var specialChar =  
+var specialChar = '`-=~!@#$%^&*()_+<>,./?:;[]{}';
+
+
+var pwdLength = prompt(`How long would you like your password?`);
+console.log(pwdLength);
+
+var selUpper = confirm(`Would you like there to be UPPERCASE characters?`);
+console.log(selUpper);
+
+var selLower = confirm(`Would you like there to be lowercase characters?`);
+console.log(selLower);
+
+var selNumbers = confirm(`Would you like to use numbers?`);
+console.log(selNumbers);
+
+var selSpecial = confirm(`Would you like to use special characters?`);
+console.log(selSpecial);
+
+//empty string of chracters I can use in my password
+var selectedCharacters = ''; //if prompts are true, add them to the array of usable characters
+
+//if true, add to my selectedCharacters string. if false, don't add.
+if (selUpper) {
+  selectedCharacters += upperCASE;
+}
+console.log(selectedCharacters);
+
+if (selLower) {
+  selectedCharacters += lowerCase;
+}
+console.log(selectedCharacters);
+
+if (selNumbers) {
+  selectedCharacters += numbers;
+}
+console.log(selectedCharacters);
+
+if (selSpecial) {
+  selectedCharacters += specialChar;
+}
+console.log(selectedCharacters);
+console.log(selectedCharacters.length)
+
+//picks a random index number from the string:selectedCharacters
+// var randomCharacter = selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)];
+// console.log(randomCharacter);
+
+var newPwd = '';
+console.log(newPwd)
+
+while (newPwd.length < 10) {
+  var randomCharacter = selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)]; //put this inside the while loop so it runs for that many times. when it is outside, it only runs the one time so it keeps adding the same value
+  newPwd += randomCharacter;
+}
+console.log(newPwd)
+
